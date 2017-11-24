da78df62649598c433e0262255792b1e08ee2397
kbpsystem <koleen.bp@outlook.com>
2017-11-25 02:49:56 +0800

    redesign using bulma.css

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
-  $('#bpi-navbar').css('background-color', 'rgb(137,3,4)');
-  $('#bpi-search').css('font-color', 'white');
-})
+  $('level-item').css('color', 'rgb(137,3,4)');
+  
+});
