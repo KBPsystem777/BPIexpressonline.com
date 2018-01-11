777d855cf7aaa8f0f79ba11ce96cda2948621fd5
KBPsystem777 <koleen.bp@outlook.com>
2018-01-11 21:24:34 +0800

    Username and Password Logic

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
-function refresh() {
-  
+function accountsLogin() {
+  var pw = document.getElementById("password");
+  var userName = document.getElementById("username");
+  if (pw.value == "434" && userName.value == "username777") {
+    location.reload();
+  } else {
+    alert("Incorrect Username or Password!");
+  }
 }
