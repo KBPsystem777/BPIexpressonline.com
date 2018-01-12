d71b20551f518d0200753beb1880f97fe8f89c0a
KBPsystem777 <koleen.bp@outlook.com>
2018-01-12 22:50:31 +0800

    Finishing Accounts page

--- js/index.js
+++ js/index.js
@@ -2,7 +2,8 @@ function accountsLogin() {
   var pw = document.getElementById("password");
   var userName = document.getElementById("username");
   if (pw.value == "434" && userName.value == "username777") {
-    location.reload();
+    location.href = "../index.html";
+    console.log("Login Success!");
   } else {
     alert("Incorrect Username or Password!");
   }
